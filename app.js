f8325b2710f7cec73613d94a7e4100e12bace711
horaleon1 <horaleon1@gmail.com>
2019-08-07 12:47:25 -0500

    first

---
+++ app.js
@@ -0,0 +1,28 @@
+//justWood Vibes :)
+
+const express = require('express');
+const morgan     = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose   = require('mongoose');
+
+const app = express();
+
+//Connect
+mongoose.connect('mongodb://localhost/miAmbulancia')
+.then( db => console.log('Mongo esta conectado'))
+.catch( err => console.log(err));
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+//Routes
+app.use('/user',require('./routes/user'))
+
+//Static
+app.use(express.static('public'));
+
+
+//Server is listening
+const PORT = process.env.PORT || 3041
+app.listen(PORT, () => console.info(`Server has started on ${PORT}`))
