ef61dcaf02d6fbbed100a157e7120cf65a98f031
horaleon1 <horaleon1@gmail.com>
2019-08-07 22:34:37 -0500

    "create UserPublicModel done and ready to use"

---
+++ models/UserPublic.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  basicInformation: {
+    role: Number,
+    fullName: {
+      name: String,
+      lastName: String
+    },
+    email: String,
+    cellphone: String
+  },
+  complementaryInformation: {
+    age: String,
+    gender: String,
+    maritalStatus: String,
+    occupation: String,
+    curp: String,
+    address: {
+      street: String,
+      number: String,
+      interiorNumber: String,
+      neighborhood: String,
+      postalCode: String,
+      city: String
+    },
+    familyContact: {
+      fullName: {
+        name: String,
+        lastName: String
+      },
+      cellphone: String
+    },
+    medicalInformation: {
+      allergies: String,
+      bloodType: String,
+      inUseMedicine: String,
+      isSmoker: Boolean,
+      isDrinker: Boolean,
+      vaccines: String,
+      weight: String,
+      height: String,
+      familySickness: String,
+      surgeries: String
+    },
+    familyCircle: {
+      member1: {
+        fullName: {
+          name: String,
+          lastName: String
+        },
+        cellphone: String,
+        relationship: String
+      },
+      member2: {
+        fullName: {
+          name: String,
+          lastName: String
+        },
+        cellphone: String,
+        relationship: String
+      },
+      member3: {
+        fullName: {
+          name: String,
+          lastName: String
+        },
+        cellphone: String,
+        relationship: String
+      }
+    }
+  }
+});
+
+const UserPublic = mongoose.model("UserPublic", UserSchema);
+module.exports = UserPublic;
